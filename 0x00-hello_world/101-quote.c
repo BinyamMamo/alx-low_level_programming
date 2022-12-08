6b4351bfb1838bcc666ca03a1107c87779d01fa6
BinyamMamo <binyammamo01@gmail.com>
2022-12-08 07:07:05 -0800

    Thu 08 Dec 2022 07:07:05 AM PST

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+fprintf(stder, "and that piece of art is useful");
+fprintf(stder, "\" - Dora Korpar, 2015-10-19\n");
+return (1);
+}
