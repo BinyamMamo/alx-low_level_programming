689f3b9021a14718685c210cac6cef793a82b39f
BinyamMamo <binyammamo01@gmail.com>
2022-12-08 06:09:12 -0800

    Thu 08 Dec 2022 06:09:12 AM PST

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 
+/*
+ *program that puts "Programming is like building a multilingual puzzle" into std
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
