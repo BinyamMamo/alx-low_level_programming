f6bf125a23543d993cf17788ee95fccb66cc777f
BinyamMamo <binyammamo01@gmail.com>
2022-12-09 20:23:22 +0300

    Fri Dec  9 20:23:22 EAST 2022

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
 
-/*
- * main - entry point
- * Return: 0 if succesful
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-for(int i=97; i<=122; i++)
- putchar((char)i); 
-for(int i=65; i<=90; i++)
- putchar((char)i); 
+int i;
+for (i = 97; i <= 122; i++)
+putchar((char)i);
+for (i = 65; i <= 90; i++)
+putchar((char)i);
 putchar('\n');
 return (0);
 }
