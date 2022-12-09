3f66110f97114e4abbc968bee8210d8e6ba3c72c
BinyamMamo <binyammamo01@gmail.com>
2022-12-09 22:46:02 +0300

    Fri Dec  9 22:46:02 EAST 2022

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -14,18 +14,22 @@ for (ii = 48; ii <= 57; ii++)
 {
 for (i = 48; i <= 57; i++)
 {
+j=i+1;
 for (jj = ii; jj <= 57; jj++)
 {
-for (j = i + 1; j <= 57; j++)
+for (; j <= 57; j++)
 {
 putchar((char)ii);
 putchar((char)i);
 putchar(' ');
 putchar((char)jj);
 putchar((char)j);
+if(ii == 57 && i == 56)
+continue;
 putchar(',');
 putchar(' ');
 }
+j=48;
 }
 }
 }
