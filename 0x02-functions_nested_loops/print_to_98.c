6e886ef8abc1de13e714da6c141da06ae406f582
BinyamMamo <binyammamo01@gmail.com>
2022-12-13 13:55:52 +0300

    Tue Dec 13 13:55:52 EAST 2022

--- 0x02-functions_nested_loops/print_to_98.c
+++ 0x02-functions_nested_loops/print_to_98.c
@@ -1,27 +1,75 @@
 #include<stdio.h>
-#include<string.h>
 #include "alx.h"
 
-void pw(int);
+int pw(int, int);
+void pint(int);
+int len(int);
+
+/**
+ * pint - prints each character of a number
+ * using the putchar function in stdio.h library
+ * @num: the base number
+ * @num: the number to be printed
+ * Return: void
+ */
+
+void pint(int num)
+{
+if (num < 10)
+putchar('0' + num);
+else
+{
+int l = len(num);
+int i = l - 1;
+int ch;
+for (; i >= 0; i--)
+{
+ch = num / pw(10, i);
+num = num % pw(10, i);
+putchar('0' + ch);
+}
+}
+}
+
+/**
+ * len - computes the length of digits
+ * of an integer number
+ * @n: the number whose number of digits
+ * is to be calculated
+ * Return: the number of digits of n
+ */
+
+int len(int n)
+{
+int i;
+for (i = 1; i <= 10; i++)
+{
+if ((n / pw(10, i)) == 0)
+break;
+}
+return (i);
+}
+
+/**
+ * pw - opt for the pow() function in math.h lib
+ * @n: the base number
+ * @p: the power
+ * Return: n to the power of p
+ */
+
+int pw(int n, int p)
+{
+int i = 0, r = 1;
+for (; i < p; i++)
+r = r *n;
+return (r);
+}
+
 /**
  * print_to_98 - prints natural numbers from n to 98
  * @n: the starting number
  * Return: void
  */
-
-void main(void)
-{
-pw(95451);
-}
-
-void pw(int n)
-{
-int x = 10;
-char numstr[x];
-sprintf(numstr, "%d", n);
-int numlen = strlen(numstr);
-printf("%d\n", numlen);
-}
 
 void print_to_98(int n)
 {
@@ -30,30 +78,22 @@ if (n > 98)
 x = -1;
 if (n < 98)
 x = 1;
-while(n < 98 || n > 98)
+while (n < 98 || n > 98)
 {
-
-if(n < 10)
-putchar('0' + n);
-
-if(n >= 10 && n < 100)
+int i = 1;
+if (n < 0)
 {
-putchar('0' + (n / 10));
-putchar('0' + (n % 10));
+i = -1;
+n = n *i;
+putchar('-');
 }
 
-if(n >= 100)
-{
-putchar('0' + (n / 100));
-putchar('0' + ((n / 10) - ((n / 100) * 10)));
-putchar('0' + (n % 10));
-}
-
+pint(n);
 putchar(',');
 putchar(' ');
-n+=x;
-} // end loop
+n = (n *i) +x;
+}
 putchar('9');
 putchar('8');
 putchar('\n');
-} //end function
+}
