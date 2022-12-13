262f1bcdd55b689f4a536650126a033713196205
BinyamMamo <binyammamo01@gmail.com>
2022-12-13 12:12:37 +0300

    Tue Dec 13 12:12:36 EAST 2022

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,4 +1,4 @@
-#include "alx.h"
+#include "main.h"
 
 /**
  * times_table - prints the time table
@@ -12,18 +12,19 @@ for (j = 0; j < 10; j++)
 {
 for (i = 0; i <= 9; i++)
 {
+if (i != 0)
+{
+_putchar(',');
+_putchar(' ');
+if ((i * j) < 10)
+_putchar(' ');
+}
 if ((j * i) < 10)
 _putchar((j * i) + '0');
 if ((j * i) >= 10)
 {
 _putchar(((j * i) / 10) + '0');
 _putchar(((j * i) % 10) + '0');
-}
-if (i != 9)
-{
-_putchar(',');
-_putchar(' ');
-_putchar(' ');
 }
 }
 _putchar('$');
