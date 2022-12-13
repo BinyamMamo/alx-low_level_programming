229413194003d819f442d0fd1efd64f1bb3dfe13
BinyamMamo <binyammamo01@gmail.com>
2022-12-13 15:06:40 +0300

    Tue Dec 13 15:06:39 EAST 2022

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ * Return: 0 if successful
+ */
+int main(void)
+{
+int a = 1;
+int b = 2;
+int i = 0;
+printf("%d", a);
+for (; i < 49; i++)
+{
+printf(", %d", b);
+int temp = b;
+b = a + b;
+a = temp;
+}
+printf("\n");
+return (0);
+}
