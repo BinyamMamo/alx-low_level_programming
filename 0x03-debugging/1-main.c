35ba11395c5c6825ca707c776a0cfa7104c645b2
BinyamMamo <binyammamo01@gmail.com>
2022-12-14 11:30:46 +0300

    Wed Dec 14 11:30:45 EAST 2022

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -2,15 +2,22 @@
 
 
 /**
- * main - Entry point of the program
- * Return: 0 if successful
- */
+* main - prints the largest of 3 integers
+* Return: 0
+*/
 int main(void)
 {
 int i;
 
 printf("Infinite loop incoming :(\n");
+/*
+ * i = 0;
 
+ * while (i < 10)
+ * {
+ * putchar(i);
+ * }
+*/
 printf("Infinite loop avoided! \\o/\n");
 
 return (0);
