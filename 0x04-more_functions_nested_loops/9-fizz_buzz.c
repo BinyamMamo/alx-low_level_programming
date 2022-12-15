b9e7533e1a87d068656af6d598c29537b6ee4879
BinyamMamo <binyammamo01@gmail.com>
2022-12-15 13:44:51 +0300

    Thu Dec 15 13:44:50 EAST 2022

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,8 +11,10 @@ int main(void)
 int i = 1;
 for (; i <= 100; i++)
 {
-if (i != 1 && i != 100)
+if (i != 1)
+{
 printf(" ");
+}
 if (i % 3 == 0 && i % 5 == 0)
 printf("FizzBuzz");
 else if (i % 3 == 0)
