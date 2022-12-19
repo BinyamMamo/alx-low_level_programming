5886e51e3b04180b30787d85061fea0be82f6a4a
BinyamMamo <binyammamo01@gmail.com>
2022-12-19 23:30:56 +0300

    ሰኞ፣ ዲሴምበር 19 ቀን 11:30:56 ከሰዓት EAT 2022 ዓ/ም

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,6 +8,7 @@ int len(char *str);
 void print_rev(char *str)
 {
 	int i = len(str) - 1;
+
 	for (; i >= 0; --i)
 	{
 		putchar(str[i]);
@@ -18,13 +19,14 @@ void print_rev(char *str)
  * len - finds the length of a string
  * @str: a character array to store the input
  * Return: the length of str
- */ 
+ */
 int len(char *str)
 {
-  int c = 0;
-  while (str[c] != '\0')
-  {
-  	c++;
-  }
-  return (c);
+	int c = 0;
+
+	while (str[c] != '\0')
+	{
+		c++;
+	}
+	return (c);
 }
