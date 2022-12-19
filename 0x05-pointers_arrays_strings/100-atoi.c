9d4107518c0d8f3633a7e50c53e7631e5b17df76
BinyamMamo <binyammamo01@gmail.com>
2022-12-20 00:20:32 +0300

    ማክሰኞ፣ ዲሴምበር 20 ቀን 12:20:32 ጡዋት EAT 2022 ዓ/ም

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <limits.h>
 int len(char *);
-int pw(int,int);
+int pw(int, int);
 /**
  * pw - finds the power of n to p
  * @n: base number
@@ -12,7 +12,8 @@ int pw(int n, int  p)
 {
 	int j = 0;
 	int r = 1;
-	for(; j < p; j++)
+
+	for (; j < p; j++)
 	{
 		r = r * n;
 	}
@@ -23,36 +24,40 @@ int pw(int n, int  p)
  * len - finds the length of string
  * @str: a character array to store the input
  * Return: the length of str
- */ 
+ */
 int len(char *str)
 {
 	int c = 0;
-  while (str[c] != '\0')
-  {
-  	c++;
-  }
-  return (c);
+
+	while (str[c] != '\0')
+	{
+		c++;
+	}
+	return (c);
 }
 
 
 /**
  * _atoi - parses integer from a string
- * @*str: string to be converted to integer
+ * @str: string to be converted to integer
  * Return: the converted integer
  */
 int _atoi(char *str)
 {
-	int i = len(str)-1;
+	int i = len(str) - 1;
 	int num = 0;
-	int k = 0;	
+	int k = 0;
+
 	for (; i >= 0; --i)
 	{
 		if (str[i] >= 48 && str[i] <= 57)
 		{
-			int n = str[i]-48;
+			int n = str[i] - 48;
+
 			num = num + (n * pw(10, k));
 			k++;
-		} else if(num != 0 && str[i] != ' ' && str[i] != '-' && str[i] != '+')
+		}
+		else if (num != 0 && str[i] != ' ' && str[i] != '-' && str[i] != '+')
 		{
 			num = 0;
 			k = 0;
