6b7f8d9423d7933acca606b1e8baed73d7b22f76
BinyamMamo <binyammamo01@gmail.com>
2022-12-19 23:28:30 +0300

    ሰኞ፣ ዲሴምበር 19 ቀን 11:28:30 ከሰዓት EAT 2022 ዓ/ም

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,30 +1,30 @@
-#include <stdio.h>
+int len(char *);
 
-void reverse(char *s);
-int len(char *s);
-
-int main(int argc, char const *argv[])
+/**
+ * rev_string - access the string through the pointer and reverse it
+ * @str: the address of the string to reverse
+ * Return: nothing
+ */
+void rev_string(char *str)
 {
-	printf("Hello world everybody\n");
-	char str[] = "reverse";
-	reverse(str);
-	puts(str);
-	return 0;
-}
-
-void reverse(char *str)
-{
-	for (int i = 0; i < len(str)/2; i++)
+	for (int i = 0; i < len(str) / 2; i++)
 	{
 		char temp = str[i];
-		str[i] = str[(len(str) - 1) - i];	
-		str[(len(str) - 1) - i] = temp;	
+
+		str[i] = str[(len(str) - 1) - i];
+		str[(len(str) - 1) - i] = temp;
 	}
 }
 
+/**
+ * len - finds the length of string
+ * @str: a character array to store the input
+ * Return: the length of str
+ */
 int len(char *str)
 {
 	int c = 0;
+
 	for (int i = 0; str[i] != '\0'; ++i)
 	{
 		c++;
