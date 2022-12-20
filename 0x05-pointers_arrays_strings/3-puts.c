e413896c8d873ea5b7627eaa53a8a7bb5c6bd749
BinyamMamo <binyammamo01@gmail.com>
2022-12-20 18:03:37 +0300

    ማክሰኞ፣ ዲሴምበር 20 ቀን  6:03:37 ከሰዓት EAT 2022 ዓ/ም

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -6,7 +6,8 @@
  * @str: string to print
  * Return: nothing
  */
-void _puts(char *str)
+
+int _puts(char *str)
 {
 	int i = 0;
 
@@ -15,4 +16,24 @@ void _puts(char *str)
 		putchar(str[i]);
 	}
 	putchar('\n');
+}
+
+/**
+ * _puts - prints string
+ * @str: string to print
+ * Return: nothing
+ */
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char *str;
+
+    str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
+    _puts(str);
+    return (0);
 }
