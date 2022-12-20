de81286e6f5d565319bb3aa8bde84738b4e454f8
BinyamMamo <binyammamo01@gmail.com>
2022-12-20 18:17:34 +0300

    ማክሰኞ፣ ዲሴምበር 20 ቀን  6:17:34 ከሰዓት EAT 2022 ዓ/ም

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -9,10 +9,11 @@
 int _atoi(char *s)
 {
 	int i = 0;
+	int k = 1;
 	int num = 0;
 	int sign = 1;
 
-	while (s[i] != 0)
+	while ((s[i] < 48 || s[i] > 57) && s[i] != 0)
 	{
 		if (s[i] == '-')
 		{
@@ -25,7 +26,12 @@ int _atoi(char *s)
 	while (s[i] != 0)
 	{
 		if (s[i] > 47 && s[i] < 58)
-			num = (num * 10) + s[i] - 48;
+		{
+			k = 0;
+			num = (num * 10) + (s[i] - 48);
+		}
+		if ((s[i] < 48 || s[i] > 57) && k == 0)
+			break;
 		i++;
 	}
 
