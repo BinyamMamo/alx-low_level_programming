36bba6b11b831a7809d20bbbad4cf4b51365bbad
BinyamMamo <binyammamo01@gmail.com>
2022-12-26 08:35:11 +0300

    ሰኞ፣ ዲሴምበር 26 ቀን  8:35:11 ጡዋት EAT 2022 ዓ/ም

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,54 +1,40 @@
 #include <stdio.h>
 
 int len(char *str);
-char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void rev_string(char *str);
-
-int main(int argcac, char const *argv[])
-{
-	char s[10];
-	char n1[] = "134576225";
-	char n2[] = "213454";
-	infinite_add(n1,n2,s,2);
-	printf("Hello world\n");	
-	return 0;
-}
+char *_strcpy(char *dest, char *src);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
 
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int i = 0;
+	int i = 0, k = 0;
 	int a, b, rem, sum;
-	a = b = rem = sum = 0;
-	rev_string(n1);
-	rev_string(n2);
 	int largest = (len(n1) > len(n2))? len(n1): len(n2);
-	char str[largest + 2];
+	a = b = rem = sum = 0;
 
-	while (!(i >= len(n1) && i >= len(n2)))
+	i = largest - 1;
+
+	while (i >= 0)
 	{
-		
-		if(i >= len(n1))
+		if (i > len(n1) - 1)
 			a = 0;
 		else
 			a = n1[i] - 48;
-		
-		if(i >= len(n2))
+
+		if (i > len(n2) - 1)
 			b = 0;
 		else
 			b = n2[i] - 48;
-		
+
 		sum = (a + b + rem) % 10;
 		rem = (a + b + rem) / 10;
-		str[i] = sum + 48;
-
-		printf("a: %d b: %d sum: %d rem: %d str[%d]: %d len: %d\n", a, b , sum ,rem, i, str[i], len(n1));
-		i++;
+		r[k] = sum + 48;
+		i--;
+		k++;
 	}
-	str[i] = 0;
-	if(len(str) > size_r)
-		printf("ERROR: 0\n");
-	rev_string(str);
-	printf("sum: %s\n", str);
+	r[k] = 0;
+	rev_string(r);
+
 	return (r);
 }
 
@@ -86,4 +72,22 @@ void rev_string(char *str)
 		str[i] = str[(len(str) - 1) - i];
 		str[(len(str) - 1) - i] = temp;
 	}
+}
+
+/**
+ * *_strcpy - copies string
+ * @dest: a string to store the copied string
+ * @src: string to copy
+ * Return: the copied string(dest)
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	for (; i < len(src); ++i)
+	{
+		dest[i] = src[i];
+	}
+	dest[len(src)] = '\0';
+	return (dest);
 }
