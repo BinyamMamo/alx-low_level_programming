9e7fc7a969ebf64e4c2c7cd46d516e05bc7d7aaf
BinyamMamo <binyammamo01@gmail.com>
2023-01-03 03:27:48 +0300

    memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -3,6 +3,7 @@
  * @s: s
  * @b: b
  * @n: n
+ *
  * Return: r
  */
 char *_memset(char *s, char b, unsigned int n)
@@ -10,10 +11,11 @@ char *_memset(char *s, char b, unsigned int n)
 unsigned int i;
 
 i = 0;
-while (i < n && s[i] != 0)
+while (i < n)
 {
 s[i] = b;
 i++;
 }
+
 return (s);
 }
