dedfea92f9ac7b50c9293bfffed55a07a85cf19e
BinyamMamo <binyammamo01@gmail.com>
2023-01-03 03:42:00 +0300

    strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,19 @@
+char *_strchr(char *s, char c)
+{
+int i, state;
+
+i = 0;
+state = 0;
+while (s[i] != 0)
+{
+if (s[i] == c)
+{
+state = 1;
+break;
+}
+i++;
+}
+if (state)
+return (&s[i]);
+return (NULL);
+}
