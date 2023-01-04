6e8051043c11122efcef000f9b512147f1229fc6
BinyamMamo <117805811+BinyamMamo@users.noreply.github.com>
2023-01-04 04:43:38 +0200

    Update 3-strspn.c

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * _strspn - _strspn
+ * @s: first input
+ * @accept: second input
+ *
+ * Return: pointer to string
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	unsigned int index, i, j;
+
+	index = 0;
+	i = 0;
+	while (s[i] != 0)
+	{
+		j = 0;
+
+		for (; accept[j] != 0; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				index = i;
+				break;
+			}
+		}
+		if (index != -1)
+		{
+			break;
+		}
+		i++;
+	}
+	return (index);
+}
