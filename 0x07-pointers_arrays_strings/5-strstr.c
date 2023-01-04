b7f6f3c9d9245a7319e79c8fbf42080748c7a2e9
BinyamMamo <117805811+BinyamMamo@users.noreply.github.com>
2023-01-04 04:08:57 +0200

    Create 5-strstr.c

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,34 @@
+/**
+ * _strstr - _strstr
+ * @haystack: first input
+ * @needle: second input
+ * 
+ * Return: pointer to string
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int index, i, j, state;
+	
+	index = -1;
+	i = 0;
+	while (haystack[i] != 0)
+	{
+		j = 0;
+		state = 0;
+		
+		for (; needle[j] != 0; j++)
+		{
+			if (haystack[i + j] == needle[j] || haystack[i + j] == needle[j] - 32)
+			{
+				state++;
+			}
+		}
+		if (state == j)
+		{
+			index = i;
+			break;
+		}
+		i++;
+	}
+	return &haystack[index];
+}
