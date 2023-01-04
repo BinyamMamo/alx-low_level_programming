1d86814c0dda3502c022dba08c10a41f56959a06
BinyamMamo <117805811+BinyamMamo@users.noreply.github.com>
2023-01-04 06:03:23 +0200

    Update 3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+
 /**
 * _strspn - _strspn
 * @s: first input
@@ -8,26 +10,26 @@
 unsigned int _strspn(char *s, char *accept)
 {
 	unsigned int index, i, j;
-
+	
 	index = 0;
-	j = 0;
-
-	for (; accept[j] != 0; j++)
+	i = 0;
+	while (s[i] != 0)
 	{
-		i = 0;
-
-		while (s[i] != 0)
+		j = 0;
+		
+		for (; accept[j] != 0; j++)
 		{
 			if (s[i] == accept[j])
 			{
-				if (i > index)
-					index = i;
+				index = i;
 				break;
 			}
-
-			i++;
 		}
+		if (index != 0)
+		{
+			break;
+		}
+		i++;
 	}
-
-	return (index + 1);
+	return (index);
 }
