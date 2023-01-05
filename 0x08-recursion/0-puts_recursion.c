e74767520e801e56f86908389e1a19bc1042ceb5
BinyamMamo <117805811+BinyamMamo@users.noreply.github.com>
2023-01-05 03:26:10 +0200

    Update 0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,1 +1,14 @@
-
+/**
+* _puts_recursion - _puts_recursion
+* @s: first input
+*
+* Return: nothing
+*/
+void _puts_recursion(char *s)
+{
+	if (*s != '\0')
+		_putchar(*s);
+	else
+		return;
+	_puts_recursion(s + 1);
+}
