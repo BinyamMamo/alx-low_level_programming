c30b74323deb36e5b954c19b8509d55695ce4516
BinyamMamo <117805811+BinyamMamo@users.noreply.github.com>
2023-01-05 03:44:43 +0200

    Create 4-pow_recursion.c

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,13 @@
+/**
+* _pow_recursion - _pow_recursion
+* @x: first input
+* @y: second input
+*
+* Return: 
+*/
+int _pow_recursion(int x, int y)
+{
+	if (y == 0)
+		return 1;
+	return x * _pow_recursion(x, y - 1);
+}
