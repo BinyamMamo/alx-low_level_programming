3a06f76ded2208b154bc82dc4683bba85853070b
BinyamMamo <117805811+BinyamMamo@users.noreply.github.com>
2023-01-05 04:21:21 +0200

    Create 101-wildcmp.c

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,35 @@
+/**
+* wildcmp - wildcmp
+* @s1: first input
+* @s2: second input
+* Return: pointer
+*/
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == '*' && *s2 == '*')
+	{
+		return (wildcmp(s1, s2));
+	}
+
+	else if (*s1 == '*' && *s2 != '*')
+	{
+		if (*s2 != 0 && *s1 != '*')
+			return (wildcmp(s1, s2 + 1));
+	}
+	
+	else if (*s2 == '*')
+	{
+		if (*s1 != 0)
+			return (wildcmp(s1 + 1, s2 ));
+	}
+	
+	else if (*s1 == *s2)
+	{
+		if (*s1 != 0 && *s2 != 0)
+			return (wildcmp(s1 + 1, s2 + 1));
+	}
+	else
+			return(0);
+	
+	return (1);
+}
