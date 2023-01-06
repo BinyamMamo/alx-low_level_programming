f2089ad83f1a57e0bcdf654649c4ed574bdb8f79
BinyamMamo <117805811+BinyamMamo@users.noreply.github.com>
2023-01-06 04:01:51 +0200

    Create 6-is_prime_number.c

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,36 @@
+int prime(int num, int x);
+
+/**
+* prime - prime
+* @num: first input
+* @x: second input
+* Return: number of factors
+*/
+int prime(int num, int x)
+{
+	int i;
+
+	i = 0;
+	if (x > 1)
+	{
+		i = prime(num, x - 1);
+		if (num % x == 0)
+		{
+			i++;
+		}
+	}
+	return i;
+}
+
+/**
+* is_prime_number - is_prime_number
+* @n: first input
+*
+* Return: 1 or 0
+*/
+int is_prime_number(int n)
+{
+	if (prime(n, n - 1) > 0)
+		return (1);
+	return (0);
+}
