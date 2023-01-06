a3ed1862fc8ba85a6a2b16e762dadfddb298ae9f
BinyamMamo <117805811+BinyamMamo@users.noreply.github.com>
2023-01-06 04:45:52 +0200

    Create 100-is_palindrome.c

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+char prime(char *str, int x);
+
+/**
+* prime - prime
+* @str: first input
+* @x: second input
+* Return: number of factors
+*/
+char prime(char *str, int x)
+{
+	int i;
+	
+	i = 0;
+	if (*str == 0)
+	{
+		return *(str - x);
+	}
+	i = prime(str + 1, x);	
+
+	return i;
+}
+
+/**
+* is_palindrome - is_palindrome
+* @s: first input
+*
+* Return: palindrome
+*/
+int is_palindrome(char *s)
+{
+	int i;
+	char c;
+	i = 0;
+	if (*s != 0)
+	{
+		i = is_palindrome(s + 1) + 1;
+		c = prime(s, i);
+		if (*s != c)
+			return -1;
+	}
+	return (i);
+}
