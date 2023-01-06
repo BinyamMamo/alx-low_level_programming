9fe6920caa118a77942bff4d35b0a08537a94a87
BinyamMamo <117805811+BinyamMamo@users.noreply.github.com>
2023-01-06 03:36:46 +0200

    Create 5-sqrt_recursion.c

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,37 @@
+/**
+* sqr - sqr
+* @num: first input
+* @x: second input
+*
+* Return: sqrt of a number
+*/
+int sqr(int num, int x)
+{
+	int i;
+
+	i = 0;
+	if (num == 0)
+		return (1);
+	if (x > 0)
+	{
+		if ((num % x == 0) && (num == (x * x)))
+		{
+			i = 1;
+			return (i); 
+		}
+		else
+			i = sqr(num, x - 1);
+	}
+	return (i);
+}
+
+/**
+* _sqrt_recursion - _sqrt_recursion
+* @n: first input
+*
+* Return: sqrt of a number
+*/
+int _sqrt_recursion(int n)
+{
+	return sqr(n, n/2);
+}
