fcee1ac70f73e0a7aaebcf52b7cbd0f959563898
BinyamMamo <117805811+BinyamMamo@users.noreply.github.com>
2023-01-06 03:03:08 +0200

    Update 1-print_rev_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,32 +1,19 @@
+#include <stdio.h>
 #include "main.h"
 
-int len(char *s);
-
 /**
-* _print_rev_recursion - _print_rev_recursion
+* _print_rev_recursion - _puts_recursion
 * @s: first input
+*
 * Return: nothing
 */
 void _print_rev_recursion(char *s)
 {
-	_putchar(*(s + len(s) - 1));
-	if (len(s) > 1)
-		_print_rev_recursion(s + len(s) - 1);
-}
-
-/**
-* len - _strlen_recursion
-* @s: first input
-*
-* Return: length of string
-*/
-int len(char *s)
-{
-	int c;
-
-	c = 0;
 	if (*s != '\0')
-		c = len(s + 1) + 1;
-
-	return (c);
+	{
+		_puts_recursion(s + 1);
+		_putchar(*s);
+	}
 }
+
+
