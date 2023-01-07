92dde28eb274ca9ff42aca56d9a661d573b733c1
BinyamMamo <117805811+BinyamMamo@users.noreply.github.com>
2023-01-07 03:44:23 +0200

    Create 0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ * argc: argument count
+ * argv: argument array
+ *
+ * Return: always 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s", argv[0]);
+	return (0);
+}
