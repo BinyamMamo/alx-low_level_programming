7f23366f01a71defdc23943112bbb58761393aca
BinyamMamo <117805811+BinyamMamo@users.noreply.github.com>
2023-01-07 04:14:36 +0200

    Update 3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * main - Entry point of the program
@@ -10,7 +11,7 @@
 int main(int argc, char *argv[])
 {
 	if (argc == 3)
-		printf("%d\n", (argv[1] - 48) * (argv[2] - 48));
+		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
 	else
 	{
 		printf("Error");
