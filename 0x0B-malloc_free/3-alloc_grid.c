6a00fa5ca2266004bc743422bafd474ed80e18fa
BinyamMamo <binyammamo01@gmail.com>
2023-01-11 05:47:41 +0300

    update file

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 /**
- * alloc_grid - creates an array of size @size with the character @c as its value
+ * alloc_grid - creates an array
  * @width: the size of the array
  * @height: the value to initialise the array with
  *
@@ -15,17 +15,26 @@ int i;
 int **grid;
 int *values;
 
-values = malloc(sizeof(*values) * width * height);
-grid = malloc(sizeof(*grid) * height);
+if (width <= 0 || height <= 0)
+return (NULL);
 
-for (i = 0; i < width * height; i++) {
+values = malloc(sizeof(*values) * width * height);
+if (values == NULL)
+return (NULL);
+grid = malloc(sizeof(*grid) * height);
+if (grid == NULL)
+return (NULL);
+
+for (i = 0; i < width * height; i++)
+{
 values[i] = 0;
 }
 
-for (i = 0; i < height; i++) {
+for (i = 0; i < height; i++)
+{
 grid[i] = &values[i * width];
 }
 
-return grid;
+return (grid);
 }
 
