33f5b33ddbd66e78af039c7648cd86e63a61624b
BinyamMamo <binyammamo01@gmail.com>
2023-01-11 04:26:07 +0300

    update file

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,12 +14,12 @@ char *str_concat(char *s1, char *s2)
 int i, c, j;
 char *s;
 
-if (s1 == NULL || s2 == NULL)
+if (s1 == NULL && s2 == NULL)
 return (NULL);
 c = 0;
-while (s1[c] != 0)
+while (s1[c] != 0 && s1 != NULL)
 c++;
-while (s2[c] != 0)
+while (s2[c] != 0 && s2 != NULL)
 c++;
 
 if (c == 0)
@@ -30,14 +30,14 @@ s = malloc(c + 1);
 if (s == NULL)
 return (NULL);
 i = 0;
-while (s1[i] != 0)
+while (s1[i] != 0 && s1 != NULL)
 {
 s[i] = s1[i];
 i++;
 }
 j = i;
 i = 0;
-while (s2[i] != 0)
+while (s2[i] != 0 && s2 == NULL)
 {
 s[j + i] = s2[i];
 i++;
