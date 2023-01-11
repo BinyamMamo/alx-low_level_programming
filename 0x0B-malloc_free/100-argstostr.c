3c7737a62545a275038ef3526dfef08add59fb6f
BinyamMamo <binyammamo01@gmail.com>
2023-01-12 02:13:03 +0300

    create file

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * argstostr - converts arguments to str
+ * @ac: argument count
+ * @av: argument list
+ *
+ * Return: the concatenated arguments
+ */
+
+char *argstostr(int ac, char **av)
+{
+	char *str, *s;
+	int i, c, j;
+
+	c = 0;
+	for (i = 0; i < ac; i++)
+	{
+		s = av[i];
+		for (j = 0; s[j] != 0; j++)
+			c++;
+		c++;
+	}
+
+	str = malloc(sizeof(char) * c);
+
+	if (str == NULL)
+		return NULL;
+
+	i = 0;
+	c = 0;
+	for (; i < ac; i++)
+	{
+		s = av[i];
+		j = 0;
+		for (; s[j] != 0; j++)
+		{
+			str[c] = s[j];
+			c++;
+		}
+		str[c] = '\n';
+		c++;
+	}
+
+	return str;
+}
