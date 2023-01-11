80e30ccb7d9d782d261d8b0e00597c49ce2c921f
BinyamMamo <binyammamo01@gmail.com>
2023-01-11 03:40:20 +0300

    create file

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _strdup - duplicates string array
+ * @str: string to duplicate
+ *
+ * Return: a pointer to the duplicated string
+ */
+
+char *_strdup(char *str)
+{
+int i, c;
+char *s;
+
+c = 0;
+while (str[c] != 0)
+c++;
+
+s = malloc(c + 1);
+
+i= 0;
+while (str[i] != 0) {
+s[i] = str[i];
+i++;
+}
+
+s[i] = 0;
+
+return s;
+}
+
