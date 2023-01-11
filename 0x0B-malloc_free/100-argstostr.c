ffa157ac71949c3ffc4ff5fa6177bf1b81672a2c
BinyamMamo <binyammamo01@gmail.com>
2023-01-12 02:48:06 +0300

    update file

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -13,16 +13,14 @@ char *argstostr(int ac, char **av)
 {
 	char *str, *s, *f;
 	int i, c, j;
-
-  if (ac == 0 || av == NULL)
-  {
-  	f = "Failed\n";
+	if (ac == 0 || av == NULL)
+	{
+		f = "Failed\n";
 		str = malloc((sizeof(char) * 8));
-
 		for (i = 0; i < 8; i++)
 			str[i] = f[i];
 		str[i] = '\0';
-  	return (str);
+		return (str);
   }
 	c = 0;
 	for (i = 0; i < ac; i++)
@@ -32,19 +30,14 @@ char *argstostr(int ac, char **av)
 			c++;
 		c++;
 	}
-
 	str = malloc((sizeof(char) * c) + 1);
-
 	if (str == NULL)
 		return (NULL);
-
-	i = 0;
 	c = 0;
-	for (; i < ac; i++)
+	for (i = 0; i < ac; i++)
 	{
 		s = av[i];
-		j = 0;
-		for (; s[j] != 0; j++)
+		for (j = 0; s[j] != 0; j++)
 		{
 			str[c] = s[j];
 			c++;
