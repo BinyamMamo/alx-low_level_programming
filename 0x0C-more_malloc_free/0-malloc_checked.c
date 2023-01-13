42692b81b9de44274d706b90decdef14e69a41ee
BinyamMamo <binyammamo01@gmail.com>
2023-01-13 14:59:08 +0200

    $(date)

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(void)
+/**
+ * malloc_checked - a checked or verified malloc function
+ * @b: the number of bytes to allocate
+ *
+ * Return: nothing
+ */
+void *malloc_checked(unsigned int b)
 {
-	printf("Hello world!!!\n");
+	void *mem = malloc(b);
+
+	if (mem == NULL)
+		exit(98);
+	return (mem);
 }
