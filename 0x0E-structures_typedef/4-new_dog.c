842f7dc236389cf996f337ce1665dab9525c096f
BinyamMamo <binyammamo01@gmail.com>
2023-01-16 20:45:59 +0200

    Mon, Jan 16, 2023  8:45:59 PM

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,27 @@
+#include <string.h>
+#include <stdlib.h>
+#include "dog.h"
+/**
+ * new_dog - create a new dog structure
+ * @name: the name of the dog
+ * @age: the age of the dog
+ * @owner: the owner of the dog
+ *
+ * Return: the dog structure
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t d;
+
+	d.name = malloc(sizeof(char) * strlen(name));
+	if (d.name == NULL)
+		return (NULL);
+	strcpy(d.name, name);
+	d.age = age;
+	d.owner = malloc(sizeof(char) * strlen(owner));
+	if (d.owner == NULL)
+		return (NULL);
+	strcpy(d.owner, owner);
+
+	return (&d);
+}
