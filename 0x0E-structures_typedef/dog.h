b2eb0a1debf27dddc157e3920f104f7395a9feaa
BinyamMamo <binyammamo01@gmail.com>
2023-01-16 18:56:05 +0200

    Mon, Jan 16, 2023  6:56:05 PM

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,14 @@
+/**
+ * struct dog - creates a dog object
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ * Description: creates a dog object with his name, his age and owner name
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
