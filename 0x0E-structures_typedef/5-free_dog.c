339528d4349638af5b5678c9dd439aa067a674ee
BinyamMamo <binyammamo01@gmail.com>
2023-01-17 20:31:14 +0200

    Tue, Jan 17, 2023  8:31:13 PM

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,6 +1,12 @@
 #include <stdlib.h>
 #include "dog.h"
 
+/**
+ * free_dog - cretes a free dog
+ * @d: the dog to free
+ *
+ * Return: nothing
+ */
 void free_dog(dog_t *d)
 {
 	free(d);
---
+++ objdump.c
@@ -0,0 +1,10 @@
+
+int main(void)
+{
+	int a = 4;
+	int b = 8;
+	char c = '^';
+	char d = '@';
+	char aa = 'a';
+	char A = 'A';
+}
