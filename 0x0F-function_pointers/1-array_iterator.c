65510fc135089ac65ab8f2e34a33b07ea2a73830
BinyamMamo <binyammamo01@gmail.com>
2023-01-18 20:35:39 +0300

    Wed, Jan 18, 2023  8:35:39 PM

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+/**
+ * print_name - print name
+ * @name: first input
+ * @f: second input
+ *
+ * Return: nothing
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	f(name);
+}
