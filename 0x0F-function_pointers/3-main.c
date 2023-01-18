a8fa9ad846973383d8a2a34fd3926704dae73c94
BinyamMamo <binyammamo01@gmail.com>
2023-01-19 01:14:21 +0300

    Thu, Jan 19, 2023  1:14:21 AM

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+
+/**
+ * main - Entry point of the program
+ * @argc: first input
+ * @argv: second input
+ *
+ * Return: 0 always
+ */
+int main(int argc, char *argv[])
+{
+	int (*func)(int, int);
+
+	printf("arg: %s\n", argv[0]);
+	printf("arg: %s\n", argv[1]);
+	printf("arg: %s\n", argv[2]);
+	printf("arg: %s\n", argv[3]);
+	func = get_op_func(argv[2]);
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+		return (1);
+	}
+	if (func == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+		return (1);
+	}
+	if (argv[2][0] == '/' || argv[2][0] == '%' && atoi(argv[3]) == 0)
+	{
+		printf("Error\n");
+		exit(100);
+		return (1);
+	}
+
+	printf("%d\n", func(atoi(argv[1]), atoi(argv[3])));
+
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,42 @@
+#include "3-calc.h"
+
+/**
+ * op_add - op add
+ * @a: first input
+ * @b: second input
+ *
+ * Return: int
+ */
+int op_add(int a, int b) { return (a + b); }
+/**
+ * op_sub - op sub
+ * @a: first input
+ * @b: second input
+ *
+ * Return: int
+ */
+int op_sub(int a, int b) { return (a - b); }
+/**
+ * op_mul - op mul
+ * @a: first input
+ * @b: second input
+ *
+ * Return: int
+ */
+int op_mul(int a, int b) { return (a * b); }
+/**
+ * op_div - op div
+ * @a: first input
+ * @b: second input
+ *
+ * Return: int
+ */
+int op_div(int a, int b) { return (a / b); }
+/**
+ * op_mod - op mod
+ * @a: first input
+ * @b: second input
+ *
+ * Return: int
+ */
+int op_mod(int a, int b) { return (a % b); }
