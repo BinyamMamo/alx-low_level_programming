11e4cb3d8340addba96c0c1b59ec59fc034a1017
BinyamMamo <binyammamo01@gmail.com>
2023-01-18 14:25:53 +0200

    Wed, Jan 18, 2023  2:25:53 PM

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -12,16 +12,17 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t d;
-	d.name = malloc(sizeof(char) * (strlen(name) + 1));
-	if (d.name == NULL)
+	dog_t *d;
+	d = malloc(sizeof(*d));
+	d->name = malloc(sizeof(char) * (strlen(name) + 1));
+	if (d->name == NULL)
 		return (NULL);
-	strcpy(d.name, name);
-	d.age = age;
-	d.owner = malloc(sizeof(char) * (strlen(owner) + 1));
-	if (d.owner == NULL)
+	strcpy(d->name, name);
+	d->age = age;
+	d->owner = malloc(sizeof(char) * (strlen(owner) + 1));
+	if (d->owner == NULL)
 		return (NULL);
-	strcpy(d.owner, owner);
+	strcpy(d->owner, owner);
 
-	return (&d);
+	return (d);
 }
