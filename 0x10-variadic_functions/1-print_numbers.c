eb242a7dc25acc912b77ca2a0d5ad5ef3c241081
BinyamMamo <binyammamo01@gmail.com>
2023-01-19 10:18:39 +0300

    Thu, Jan 19, 2023 10:18:38 AM

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * sum_them_all - sum them all
+ * @n: first input
+ * @...: second input
+ *
+ * Return: int
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i = 0;
+	va_list args;
+
+	if (separator == NULL || n == 0)
+		return;
+
+	va_start(args, n);
+
+	for (i = 0; i < n; i++)
+		printf("%d", va_arg(args, int));
+	if (i != n - 1)
+		printf(", ");
+
+	va_end(args);
+	printf("\n");
+}
