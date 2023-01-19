8f27731333b3bd06a681e91ec057ff29347d0079
BinyamMamo <binyammamo01@gmail.com>
2023-01-19 10:10:55 +0300

    Thu, Jan 19, 2023 10:10:55 AM

--- 0x10-variadic_functions/.bkp.c
+++ 0x10-variadic_functions/.bkp.c
@@ -1,4 +1,4 @@
-#include <stdarg.h>
+#include <stdio.h>
 
 /**
  * sum_them_all - sum them all
@@ -9,16 +9,17 @@
  */
 int sum_them_all(const unsigned int n, ...)
 {
-	unsigned int i = 0;
-	int sum = 0;
-	va_list args;
-
-	va_start(args, n);
+	unsigned int i;
+	unsigned int sum = 0;
+	const unsigned int *ptr = &n;
 
 	if (n == 0)
 		return (0);
 	for (i = 0; i < n; i++)
-		sum += va_arg(args, int);
+	{
+		ptr++;
+		sum += *ptr;
+	}
 
 	return (sum);
 }
