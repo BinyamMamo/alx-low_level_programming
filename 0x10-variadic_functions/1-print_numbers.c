8310eacd6999d04f106acdc89e672fb903b37e97
BinyamMamo <binyammamo01@gmail.com>
2023-01-19 10:24:32 +0300

    Thu, Jan 19, 2023 10:24:32 AM

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -17,7 +17,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	if (separator == NULL || n == 0)
 		return;
 
-	va_start(args, n);
+	va_start(args, (int)n);
 
 	for (i = 0; i < n; i++)
 	{
