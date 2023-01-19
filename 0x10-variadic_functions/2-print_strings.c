8fc961b7a9101789cbf04cc9a237b3ee606ec82c
BinyamMamo <binyammamo01@gmail.com>
2023-01-19 10:34:39 +0300

    Thu, Jan 19, 2023 10:34:38 AM

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -13,15 +13,22 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	unsigned int i = 0;
 	va_list args;
+	char *str;
 
-	if (separator == NULL || n == 0)
+	if (separator == NULL)
 		return;
 
 	va_start(args, n);
 
 	for (i = 0; i < n; i++)
 	{
-		printf("%d", va_arg(args, int));
+		str = va_arg(args, int);
+
+		if (str == NULL)
+			printf("(nil)");
+		else
+			printf("%d", );
+
 		if (i != n - 1)
 			printf("%s", separator);
 	}
