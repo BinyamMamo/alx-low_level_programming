228640b58e054184ac3a1d2a408183e8448a8534
BinyamMamo <binyammamo01@gmail.com>
2023-01-27 01:48:47 +0300

    Fri, Jan 27, 2023  1:48:46 AM

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,44 +1,23 @@
-        .file   "101-hello_holberton.c"
-        .text
-        .section        .rodata   
-.LC0:
-        .string "Hello, Holberton\n"
-        .text
-        .globl  main
-        .type   main, @function   
-main:
-.LFB0:
-        .cfi_startproc
-        endbr64
-        pushq   %rbp
-        .cfi_def_cfa_offset 16
-        .cfi_offset 6, -16
-        movq    %rsp, %rbp
-        .cfi_def_cfa_register 6
-        leaq    .LC0(%rip), %rdi
-        call    puts@PLT
-        nop
-        popq    %rbp
-        .cfi_def_cfa 7, 8
+bits    64
+default rel
+
+global  main
+
+extern  printf
+
+section .data
+    format      db 'Hello world!', 0xA, 0
+
+section .bss
+
+section .text
+    main:
+        sub     rsp, 8
+
+        lea     rdi, [format]
+        mov     al, 0
+        call    printf wrt ..plt
+
+        add     rsp, 8
+        sub     rax, rax
         ret
-        .cfi_endproc
-.LFE0:
-        .size   main, .-main
-        .ident  "GCC: (Ubuntu 9.4.0-1ubuntu1~20.04.1) 9.4.0"
-        .section        .note.GNU-stack,"",@progbits
-        .section        .note.gnu.property,"a"
-        .align 8
-        .long    1f - 0f
-        .long    4f - 1f
-        .long    5
-0:
-        .string  "GNU"
-1:
-        .align 8
-        .long    0xc0000002
-        .long    3f - 2f
-2:
-        .long    0x3
-3:
-        .align 8
-4:
