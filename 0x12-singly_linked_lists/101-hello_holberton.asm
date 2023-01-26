304c64e10f407fc2a74b0281a4ea333e1a3c7f14
BinyamMamo <binyammamo01@gmail.com>
2023-01-27 01:42:50 +0300

    Fri, Jan 27, 2023  1:42:49 AM

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -2,7 +2,7 @@
         .text
         .section        .rodata   
 .LC0:
-        .string "Hello, Holberton"
+        .string "Hello, Holberton\n"
         .text
         .globl  main
         .type   main, @function   
