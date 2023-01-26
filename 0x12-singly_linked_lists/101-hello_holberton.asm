e7900661d9da68cf9c36d4388611b789254b7cc0
BinyamMamo <binyammamo01@gmail.com>
2023-01-27 01:41:42 +0300

    Fri, Jan 27, 2023  1:41:41 AM

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -17,39 +17,6 @@ main:
         .cfi_def_cfa_register 6
         leaq    .LC0(%rip), %rdi
         call    puts@PLT
-        movl    $0, %eax
-        popq    %rbp
-        .cfi_def_cfa 7, 8
-        ret
-        .cfi_endproc
-.LFE0:
-        .size   main, .-main
-        .ident  "GCC: (Ubuntu 9.4.0-1ubuntu1~20.04.1) 9.4.0"
-        .section        .note.GNU-stack,"",@progbits
-        .section        .note.gnu.property,"a"
-        .align 8
-root@cd1db795c482:/# emacs hw.c
-root@cd1db795c482:/# gcc -std=gnu89 -no-pie -m64 -S hw.c -o hw.asm
-root@cd1db795c482:/# cat hw.asm
-        .file   "hw.c"
-        .text
-        .section        .rodata   
-.LC0:
-        .string "Hello, Holberton"
-        .text
-        .globl  main
-        .type   main, @function   
-main:
-.LFB0:
-        .cfi_startproc
-        endbr64
-        pushq   %rbp
-        .cfi_def_cfa_offset 16
-        .cfi_offset 6, -16
-        movq    %rsp, %rbp
-        .cfi_def_cfa_register 6
-        leaq    .LC0(%rip), %rdi
-        call    puts@PLT
         nop
         popq    %rbp
         .cfi_def_cfa 7, 8
