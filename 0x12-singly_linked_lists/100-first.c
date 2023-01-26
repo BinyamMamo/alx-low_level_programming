5ead3f76000ccc5b8231eab47df47e3827e5de57
BinyamMamo <binyammamo01@gmail.com>
2023-01-27 00:54:05 +0300

    Fri, Jan 27, 2023 12:54:04 AM

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 #include "lists.h"
 
-void before_main()
+/**
+ * before_main - a function that runs before main
+ */
+void before_main(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
