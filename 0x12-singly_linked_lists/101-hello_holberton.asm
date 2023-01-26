a0362b3fa74e899ea4b5ff44113d26c59d8c999d
BinyamMamo <binyammamo01@gmail.com>
2023-01-27 01:20:08 +0300

    Fri, Jan 27, 2023  1:20:07 AM

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,44 +1,30 @@
-        .file   "101-hello_holberton.c"
-        .text
-        .section        .rodata   
-.LC0:
-        .string "Hello, Holberton", 10
-        .text
-        .globl  main
-        .type   main, @function   
-main:
-.LFB0:
-        .cfi_startproc
-        endbr64
-        pushq   %rbp
-        .cfi_def_cfa_offset 16
-        .cfi_offset 6, -16
-        movq    %rsp, %rbp
-        .cfi_def_cfa_register 6
-        leaq    .LC0(%rip), %rdi
-        call    puts@PLT
-        movl    $0, %eax
-        popq    %rbp
-        .cfi_def_cfa 7, 8
-        ret
-        .cfi_endproc
-.LFE0:
-        .size   main, .-main
-        .ident  "GCC: (Ubuntu 9.4.0-1ubuntu1~20.04.1) 9.4.0"
-        .section        .note.GNU-stack,"",@progbits
-        .section        .note.gnu.property,"a"
-        .align 8
-        .long    1f - 0f
-        .long    4f - 1f
-        .long    5
-0:
-        .string  "GNU"
-1:
-        .align 8
-        .long    0xc0000002
-        .long    3f - 2f
-2:
-        .long    0x3
-3:
-        .align 8
-4:
+	.file	"101-hello_holberton.c"
+	.def	___main;	.scl	2;	.type	32;	.endef
+	.section .rdata,"dr"
+LC0:
+	.ascii "Hello, Holberton\0"
+	.text
+	.globl	_main
+	.def	_main;	.scl	2;	.type	32;	.endef
+_main:
+LFB10:
+	.cfi_startproc
+	pushl	%ebp
+	.cfi_def_cfa_offset 8
+	.cfi_offset 5, -8
+	movl	%esp, %ebp
+	.cfi_def_cfa_register 5
+	andl	$-16, %esp
+	subl	$16, %esp
+	call	___main
+	movl	$LC0, (%esp)
+	call	_puts
+	nop
+	leave
+	.cfi_restore 5
+	.cfi_def_cfa 4, 4
+	ret
+	.cfi_endproc
+LFE10:
+	.ident	"GCC: (MinGW.org GCC-6.3.0-1) 6.3.0"
+	.def	_puts;	.scl	2;	.type	32;	.endef
--- 0x12-singly_linked_lists/101-hello_holberton.c
+++ 0x12-singly_linked_lists/101-hello_holberton.c
@@ -1,10 +1,5 @@
 #include <stdio.h>
-
-/**
- * main - Entry point of the program
- */
 int main(void)
 {
 	printf("Hello, Holberton\n");
-	return (0);
 }
