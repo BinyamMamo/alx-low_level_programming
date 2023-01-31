71ec0246e434e24b0d3e5d055107763a2d0347a1
BinyamMamo <binyammamo01@gmail.com>
2023-02-01 01:46:43 +0300

    Wed, Feb  1, 2023  1:46:42 AM

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,15 +9,15 @@
  */
 void free_listint2(listint_t **head)
 {
-	listint_t *temp = *head;
+	listint_t *temp = *head, *node = *head;
 
 	if (head == NULL)
 		return;
 	while (*head != NULL)
 	{
-		temp = (*head)->next;
-		free(*head);
-		*head = temp;
+		temp = (node)->next;
+		free(node);
+		node = temp;
 	}
 	*head = NULL;
 }
