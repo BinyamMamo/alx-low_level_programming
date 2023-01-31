14efd211e9bc8bd41dbf559ea4e14cc8fc92577e
BinyamMamo <binyammamo01@gmail.com>
2023-01-31 15:37:06 +0300

    Tue, Jan 31, 2023  3:37:06 PM

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -4,19 +4,19 @@
 #include <string.h>
 
 /**
- * print_listint - prints the linked list
- * @h: the linked list to print
+ * listint_len - returns the length of a linked list
+ * @h: the linked list
  *
  * Return: the length of the linked list
  */
-size_t print_listint(const listint_t *h)
+size_t listint_len(const listint_t *h);
 {
 	size_t len = 0;
+	listint_t *temp = h;
 
-	while (h != 0)
+	while (temp != 0)
 	{
-		printf("%d\n", h->n);
-		h = h->next;
+		temp = temp->next;
 		len++;
 	}
 
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,6 +1,5 @@
 #ifndef LISTS_H
 #define LISTS_H
-
 #include <stdio.h>
 
 /**
@@ -16,5 +15,8 @@ typedef struct listint_s
 		int n;
 		struct listint_s *next;
 } listint_t;
+
+size_t print_listint(const listint_t *h);
+size_t listint_len(const listint_t *h);
 
 #endif
