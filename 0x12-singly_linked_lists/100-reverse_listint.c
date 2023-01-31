b17842134150899674f49063eb8f1c06ab98aeae
BinyamMamo <binyammamo01@gmail.com>
2023-02-01 02:34:46 +0300

    Wed, Feb  1, 2023  2:34:45 AM

---
+++ 0x12-singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,7 @@
+#include "lists.h"
+#include <stdlib.h>
+
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t temp;
+}
---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+
+/**
+ * reverse_listint - reverse listint
+ * @head: first input
+ *
+ * Return: listint_t
+ */
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *prev, *temp;
+
+	if (*head == NULL)
+	{
+		return (NULL);
+	}
+	prev = NULL;
+	*head = *head;
+	while (*head != NULL)
+	{
+		temp = (*head)->next;
+		(*head)->next = prev;
+		*head = temp;
+		prev = *head;
+	}
+	*head = prev;
+
+	return (*head);
+}
