e73ddc76421b31049976812dcb1a4af2a81e7dfe
BinyamMamo <binyammamo01@gmail.com>
2023-02-01 01:28:44 +0300

    Wed, Feb  1, 2023  1:28:44 AM

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -13,11 +13,21 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	unsigned int i = 0;
 	listint_t *h = *head, *temp;
 
-	if (h == NULL || index > listint_len(h))
+	if (h == NULL)
 		return (-1);
 
-	while (h != NULL && i < index)
+	if (index == 0)
 	{
+		temp = h->next;
+		free(h);
+		h = temp;
+		return (1);
+	}
+
+	while (i < index - 1)
+	{
+		if (h->next == NULL)
+			return (-1);
 		h = h->next;
 		i++;
 	}
@@ -27,24 +37,4 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	h = temp;
 
 	return (1);
-}
-
-/**
- * listint_len - returns the length of a linked list
- * @h: the linked list
- *
- * Return: the length of the linked list
- */
-size_t listint_len(const listint_t *h)
-{
-	size_t len = 0;
-	listint_t *temp = (listint_t *)h;
-
-	while (temp != 0)
-	{
-		temp = temp->next;
-		len++;
-	}
-
-	return (len);
 }
--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -12,7 +12,7 @@
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	unsigned int i = 0;
-	listint_t *h = *head, *node;
+	listint_t *node;
 
 	node = malloc(sizeof(listint_t));
 	if (node == NULL)
@@ -28,13 +28,13 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	while (i < idx - 1)
 	{
-		if (h == NULL || h->next == NULL)
+		if ((*head)->next == NULL)
 			return (NULL);
-		h = h->next;
+		*head = (*head)->next;
 		i++;
 	}
-	node->next = h->next;
-	h->next = node;
+	node->next = (*head)->next;
+	(*head)->next = node;
 
 	return (node);
 }
