b681502e45c1d5182cf746e1a5e0e12610739a9b
BinyamMamo <binyammamo01@gmail.com>
2023-02-03 05:49:53 +0300

    Fri, Feb  3, 2023  5:49:53 AM

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * flip_bits - calculates number of bits needed to
@@ -12,7 +11,7 @@
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	uint i, xor = n ^ m, b = 0;
+	unsigned long int i, xor = n ^ m, b = 0;
 
 	while (xor)
 	{
