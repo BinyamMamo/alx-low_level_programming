d340a00846c962dda3b8da827e679098f1c0ae0f
BinyamMamo <binyammamo01@gmail.com>
2023-02-03 05:16:47 +0300

    Fri, Feb  3, 2023  5:16:47 AM

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -8,37 +8,7 @@
  */
 void print_binary(unsigned long int n)
 {
-	int i, state = 0, len = 1, temp = n;
-
-	if (n <= 1)
-	{
-		_putchar(48 + n);
-		return;
-	}
-
-	while (temp)
-	{
-		temp >>= 1;
-		len++;
-	}
-
-	for (i = len; i >= 0; i--)
-	{
-		if (get_bit(n, i))
-			state = 1;
-		if (state)
-			_putchar(48 + get_bit(n, i));
-	}
-}
-
-/**
- * get_bit - get bit at any index
- * @n: first input
- * @index: second input
- *
- * Return: the bit at the specified index
- */
-int get_bit(unsigned long int n, unsigned int index)
-{
-	return (index >= 64 ? -1 : n && (n & (1 << index)));
+	if (n > 1)
+		print_binary(n >> 1);
+	_putchar(48 + (n & 1));
 }
