ed63b3138433415c32aef203efd28663c13119aa
BinyamMamo <binyammamo01@gmail.com>
2023-02-03 06:00:41 +0300

    Fri, Feb  3, 2023  6:00:41 AM

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -10,6 +10,6 @@
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-	*n = (index >= (sizeof(unsigned long int) * 8) ? *n : ~(~(*n) | (1 << index)));
-	return (index >= (sizeof(unsigned long int) * 8) ? -1 : 1);
+	*n = (index >= (sizeof(ulint) * 8) ? *n : ~(~(*n) | (1 << index)));
+	return (index >= (sizeof(ulint) * 8) ? -1 : 1);
 }
