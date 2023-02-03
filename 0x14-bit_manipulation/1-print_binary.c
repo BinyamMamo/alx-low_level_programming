728eb8c13725b47d913029555d870d041267cba5
BinyamMamo <binyammamo01@gmail.com>
2023-02-03 04:21:18 +0300

    Fri, Feb  3, 2023  4:21:17 AM

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,4 +1,8 @@
 #include "main.h"
+#include <unistd.h>
+
+int num_len(unsigned int n);
+int print_uint(unsigned int n);
 
 /**
  * print_binary - print binary
@@ -24,4 +28,53 @@ void print_binary(unsigned long int n)
 int get_bit(unsigned long int n, unsigned int index)
 {
 	return (index >= 32 ? -1 : n && (n & (1 << index)));
+}
+
+int num_len(unsigned int n)
+{
+	int i = 0;
+
+	while (n != 0)
+	{
+		n /= 10;
+		i++;
+	}
+	if (i == 0)
+		return (0);
+	else
+		return (--i);
+}
+
+/**
+ * pw - opt for the pow() function in math.h lib
+ * @n: the base number
+ * @p: the power
+ * Return: n to the power of p
+ */
+
+unsigned int pw(unsigned int n, unsigned int p)
+{
+	int i = 0, r = 1;
+	for (; i < p; i++)
+		r = r * n;
+	return (r);
+}
+
+int print_uint(unsigned int n)
+{
+	int i = 0, len = num_len(n), count = 0;
+	unsigned int pow = 1;
+	char c;
+
+	for (i = len; i >= 0; i--)
+	{
+		pow = pw(10, (unsigned int)i);
+
+		c = (n / pow) + 48;
+		n = n % pow;
+		write(1, &c, 1);
+		count++;
+	}
+
+	return (count);
 }
--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -9,6 +9,6 @@
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	*n |= (index >= 32 ? *n : (1 << index));
+	*n = (index >= 32 ? *n : *n | (1 << index));
 	return (index >= 32 ? -1 : 1);
 }
