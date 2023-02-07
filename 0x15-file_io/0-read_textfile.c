3244c896b0da0103d9c6865d28724d9ec94a9628
BinyamMamo <binyammamo01@gmail.com>
2023-02-07 06:01:16 +0300

    Tue, Feb  7, 2023  6:01:16 AM

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <sys/stat.h>
+#include <stdlib.h>
 
 #ifndef STDOUT_FILENO
 #define STDOUT_FILENO 1
@@ -19,30 +20,30 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
-	ssize_t lenr, written;
-	char *buff;
+	ssize_t read_chars, written;
+	char *buffer;
 
 	if (filename == NULL)
 		return (0);
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
-	buff = malloc(sizeof(char) * letters);
-	if (buff == NULL)
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
 	{
 		close(fd);
 		return (0);
 	}
-	lenr = read(fd, buff, letters);
+	read_chars = read(fd, buffer, letters);
 	close(fd);
-	if (lenr == -1)
+	if (read_chars == -1)
 	{
-		free(buff);
+		free(buffer);
 		return (0);
 	}
-	written = write(STDOUT_FILENO, buff, lenr);
-	free(buff);
-	if (lenr != written)
+	written = write(STDOUT_FILENO, buffer, read_chars);
+	free(buffer);
+	if (read_chars != written)
 		return (0);
 	return (written);
 }
