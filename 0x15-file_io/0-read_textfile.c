e12e99faaef25a14a069febf1f00397867a1752b
BinyamMamo <binyammamo01@gmail.com>
2023-02-07 05:24:40 +0300

    Tue, Feb  7, 2023  5:24:40 AM

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -20,38 +20,35 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	int i = 0;
 	size_t j = 0;
 
-	/**
-	 * if file name is null return 0
-	 */
 	if (filename == NULL)
 		return (0);
 
 	file = open(filename, O_RDONLY);
-	/**
-	 * if file could not be opened return 0
-	 */
+
 	if (file == -1)
 		return (0);
 
-	while ((i = read(file, &c, 1)) != 0 && j < letters)
+	i = read(file, &c, 1);
+	if (i < 0)
+	{
+		close(file);
+		return (0);
+	}
+	while (i != 0 && j < letters)
 	{
 		if (i < 0)
 		{
 			close(file);
 			return (0);
-		} /**
-			 * if write fails it returns -1
-			 * and
-			 * if write could not write the expected amount of bytes it return 0
-			 * so checking if the return value gets below or equal to 0
-			 * will do the job of avoiding errors
-			 */
+		}
+
 		if (write(STDOUT_FILENO, &c, 1) <= 0)
 		{
 			close(file);
 			return (0);
 		}
 		j++;
+		i = read(file, &c, 1);
 	}
 	close(file);
 	return (j);
