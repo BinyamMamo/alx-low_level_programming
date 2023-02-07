ea6ce634013e7b556b4fd93a2a5ef7aa6271711c
BinyamMamo <binyammamo01@gmail.com>
2023-02-07 05:20:49 +0300

    Tue, Feb  7, 2023  5:20:49 AM

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -23,7 +23,7 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	file = open(filename, O_CREAT | O_TRUNC | O_RDWR);
+	file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	/**
 	 * if file could not be opened return -1
 	 */
