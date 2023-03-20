d70095ca104f9e66a6427d6bf1b7026fd6eb76b2
BinyamMamo <binyammamo01@gmail.com>
2023-03-21 00:20:55 +0300

    Tue, Mar 21, 2023 12:10:01 AM

---
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+
+/**
+ * insert_dnodeint_at_index - inserts node at an index
+ * @h: head of dlist
+ * @idx: node index
+ * @n: data to add
+ *
+ * Return:  the address of the new node, or NULL if it failed
+ */
+
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+}
